96a036ff4ec6a2851d340d1068ceb7c18f1bea86
yibai <404223879@qq.com>
2024-04-08 22:20:09 +0800

    sleep.c

--- lab1/sleep.c
+++ lab1/sleep.c
@@ -1,7 +1,26 @@
-#include "stdio.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <strsafe.h>
 
-int main()
-{
-    printf("hello xv6\n");
-    return 0;
+int main(int argc, char const *argv[]) {
+    if (argc != 2) {
+        fprintf(2, "argument  error");
+        exit(1);
+    }
+    //两个参数
+    sleep(atoi(argv[1]));
+    exit(0);
 }
+
+/**
+ * 命令行参数调用时写法如上所示
+ * argc -> 参数的数量
+ * argv[] -> 存储参数
+ * argv[0] -> 函数名
+ * argv[1] -> 第一个参数
+ * argv[2] -> 第二个参数
+ * .......
+ *
+ * 当只有一个参数时 argc == 2
+ * 一个是函数名，一个是参数
+*/
